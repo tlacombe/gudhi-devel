3f73431cc1ee219e9f80d86051c9f04d11ac6726
glisse <glisse@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2013-12-12 14:55:12 +0000

    Fill the Filtered* class a bit.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/code@161 636b058d-ea47-450e-bf9e-a15bfbe3eedb

--- concepts/FilteredSimplicialComplexDS.h
+++ concepts/FilteredSimplicialComplexDS.h
@@ -9,10 +9,49 @@
 
 /**
  *  \brief Data structure for representing a filtered simplicial complex.
+ *  \extends SimplicialComplexDS
  */
 
 struct FilteredSimplicialComplexDS
 {
+  /**
+   * @details Type of the filtration values
+   *
+   * @note OPTIONAL
+   */
+  typedef unspecified Filtration_value;
+  /**
+   * @details Returns the filtration value of a simplex s
+   *
+   * @note OPTIONAL
+   */
+  Filtration_value filtration_value(Simplex s);
+  /**
+   * @details Compares the filtration values of simplices s and t
+   *
+   * @return -1 if s comes before t in the filtration, +1
+   * if it comes later, and 0 if they come at the same time
+   *
+   * @note OPTIONAL
+   * @todo use an enum? Just a bool?
+   */
+  int compare_in_filtration(Simplex s, Simplex t);
 
+  /**
+   * Iterator over all simplices of a complex in the order of the filtration
+   *
+   * `value_type` must be a `Simplex`
+   */
+  typedef unspecified Filtration_simplex_iterator;
 
+  /**
+   *  Returns an iterator to the beginning of the sequence of
+   *  simplices of a complex in the order of the filtration
+   */
+  Filtration_simplex_iterator filtration_simplex_iterator_begin();
+  /**
+   *  Returns an iterator to the end of the sequence of 
+   *  simplices of a complex in the order of the filtration
+   */
+  Filtration_simplex_iterator filtration_simplex_iterator_end();
 };
