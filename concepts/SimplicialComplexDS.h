a706edf631a4a3151ce6ddb96469418ec99a660f
glisse <glisse@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2013-12-17 18:07:21 +0000

    Move Simplex_data
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/code@166 636b058d-ea47-450e-bf9e-a15bfbe3eedb

--- concepts/SimplicialComplexDS.h
+++ concepts/SimplicialComplexDS.h
@@ -60,17 +60,6 @@ struct SimplicialComplexDS
 	typedef unspecified Coboundary_simplex_iterator;
 	/// @}
 	
-	/*************************************************/		
-	/// \name Simplex extra data
-	/// 
-	/// @{
-	/**
-	 * Type of the extra space provided for each simplex
-	 * to store additional information
-	 */
-	typedef unspecified Simplex_data;
-	/// @}
-	
 	/*************************************************/	
 	/// \name Iterator constructors
 	/// @{
@@ -162,14 +151,4 @@ struct SimplicialComplexDS
 	void edge_contraction(Simplex s);
 	/// @}
 	/*************************************************/	
-
-	/// \name Simplex extra data accessor
-	/// @{
-	/**
-	 * Returns a pointer to the extra information provided for
-	 * the simplex s.  If not `Simplex_data` is not `void`, it must be
-	 * persistent, i.e. it cannot be part of a temporary Simplex object.
-	 */
-	Simplex_data *simplex_data_pointer(Simplex s);
-	/// @}
 };
