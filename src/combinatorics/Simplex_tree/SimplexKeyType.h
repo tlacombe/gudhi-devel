4148de83d8c2e369b8b270eeb7652b54b9ee177c
cmaria <cmaria@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2014-04-02 14:55:30 +0000

    compile
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/code@227 636b058d-ea47-450e-bf9e-a15bfbe3eedb

---
+++ src/combinatorics/Simplex_tree/SimplexKeyType.h
@@ -0,0 +1,11 @@
+/*
+ *  SimplexKeyType.h
+ *  Gudhi
+ *
+ *  Created by Clément Maria on 4/1/14.
+ *  Copyright 2014 INRIA. All rights reserved.
+ *
+ */
+
+ /** \brief Must be int.*/
+ struct SimplexKeyType {}
