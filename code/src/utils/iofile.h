29a002d51980a5ac77846cbd09c2a6ba8a57d749
cmaria <cmaria@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2014-04-02 14:53:02 +0000

    Simplex_tree insert_graph uses boost graph
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi@226 636b058d-ea47-450e-bf9e-a15bfbe3eedb

--- code/src/utils/iofile.h
+++ code/src/utils/iofile.h
@@ -17,6 +17,7 @@
 #include <cstring>
 #include <list>
 #include <cassert>
+#include <boost/graph/adjacency_list.hpp>
 
 
 /**
@@ -49,9 +50,81 @@ read_points ( std::string file_name
   in_file.close();
 }
 
+/**
+ * \brief Read a graph from a file.
+ *
+ * File format: 1 simplex per line
+ * Dim1 X11 X12 ... X1d Fil1 
+ * Dim2 X21 X22 ... X2d Fil2
+ * etc
+ *
+ * The vertices must be labeled from 0 to n-1.
+ * Every simplex must appear exactly once.
+ * Simplices of dimension more than 1 are ignored.
+ */
+struct edge_filtration_tag {
+  typedef boost::edge_property_tag kind;
+};
+struct vertex_filtration_tag {
+  typedef boost::vertex_property_tag kind;
+};
 
+typedef int    Vertex_handle;
+typedef double Filtration_value;
+typedef boost::adjacency_list < boost::vecS, boost::vecS, boost::undirectedS
+                              , boost::property < vertex_filtration_tag, Filtration_value >
+                              , boost::property< edge_filtration_tag, Filtration_value > 
+                              >                   Graph_t;
+typedef std::pair< Vertex_handle, Vertex_handle > Edge_t;
+inline Graph_t
+read_graph ( std::string file_name )
+{
+  std::ifstream in_ (file_name.c_str(),std::ios::in);
+  if(!in_.is_open()) { std::cerr << "Unable to open file " << file_name << std::endl; }
 
+  std::vector< Edge_t >                       edges;
+  std::vector< Filtration_value >             edges_fil;
+  std::map< Vertex_handle, Filtration_value > vertices;
+  
+  std::string      line;
+  int              dim;
+  Vertex_handle    u,v,max_h = -1;
+  Filtration_value fil;
+  while( getline ( in_ , line ) )
+  {
+    std::istringstream iss( line );
+    while(iss >> dim) {
+      switch ( dim ) {
+        case 0 : {
+          iss >> u; iss >> fil; 
+          vertices[u] = fil;
+          if(max_h < u) { max_h = u; }
+          break;
+        }
+        case 1 : {
+          iss >> u; iss >> v; iss >> fil;
+          edges.push_back(Edge_t(u,v));
+          edges_fil.push_back(fil);
+          break;
+        }
+        default: {break;} 
+      } 
+    }
+  }
+  in_.close();
+  if(max_h+1 != vertices.size()) 
+    { std::cerr << "Error: vertices must be labeled from 0 to n-1 \n"; }
 
+  Graph_t skel_graph(edges.begin(),edges.end(),edges_fil.begin(),vertices.size());
+  auto vertex_prop = boost::get(vertex_filtration_tag(),skel_graph);
+
+  boost::graph_traits<Graph_t>::vertex_iterator vi, vi_end;
+  auto v_it = vertices.begin();
+  for (tie(vi, vi_end) = boost::vertices(skel_graph); vi != vi_end; ++vi,++v_it)
+  { boost::put(vertex_prop,*vi,v_it->second); }
+
+  return skel_graph;
+}
 
 /**
  * @brief OFF reader, save the content of the file (vertices and maximal faces) in 'complex'.
