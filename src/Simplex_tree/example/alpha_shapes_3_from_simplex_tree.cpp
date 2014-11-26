a1a41a8b19bfd5bb2d78979e869f38a08d5df9eb
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2014-11-26 15:47:09 +0000

    After classification
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi@297 636b058d-ea47-450e-bf9e-a15bfbe3eedb

--- src/Simplex_tree/example/alpha_shapes_3_from_simplex_tree.cpp
+++ src/Simplex_tree/example/alpha_shapes_3_from_simplex_tree.cpp
@@ -4,37 +4,43 @@
 #include <CGAL/iterator.h>
 
 #include <fstream>
-#include <list>
-#include <cassert>
 
-#include <vector>
-#include <boost/variant.hpp>
-#include <boost/optional.hpp>
 
+// Alpha_shape_3 templates type definitions
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+typedef CGAL::Alpha_shape_vertex_base_3<Kernel>             Vb;
+typedef CGAL::Alpha_shape_cell_base_3<Kernel>               Fb;
+typedef CGAL::Triangulation_data_structure_3<Vb,Fb>         Tds;
+typedef CGAL::Delaunay_triangulation_3<Kernel,Tds>          Triangulation_3;
+typedef CGAL::Alpha_shape_3<Triangulation_3>                Alpha_shape_3;
 
-typedef CGAL::Alpha_shape_vertex_base_3<Kernel>          Vb;
-typedef CGAL::Alpha_shape_cell_base_3<Kernel>            Fb;
-typedef CGAL::Triangulation_data_structure_3<Vb,Fb>  Tds;
-typedef CGAL::Delaunay_triangulation_3<Kernel,Tds>       Triangulation_3;
-typedef CGAL::Alpha_shape_3<Triangulation_3>         Alpha_shape_3;
+// From file type definition
+typedef Kernel::Point_3                                     Point;
 
-typedef Kernel::Point_3                                  Point;
-typedef Alpha_shape_3::Alpha_iterator               Alpha_iterator;
-
+// filtration with alpha values needed type definition
+typedef Alpha_shape_3::FT Alpha_value_type;
+typedef CGAL::Object      Object;
+typedef CGAL::Dispatch_output_iterator<
+  CGAL::cpp11::tuple<Object, Alpha_value_type>,
+  CGAL::cpp11::tuple<std::back_insert_iterator< std::vector<Object> >, std::back_insert_iterator< std::vector<Alpha_value_type> >
+                     > > Dispatch;
+typedef Alpha_shape_3::Cell_handle   Cell_handle;
+typedef Alpha_shape_3::Facet         Facet;
+typedef Alpha_shape_3::Edge          Edge;
+typedef Alpha_shape_3::Vertex_handle Vertex_handle;
 
 int main (int argc, char * const argv[])
 {
+	// program args management
   if (argc != 2) {
     std::cerr << "Usage: " << argv[0]
       << " path_to_file_graph \n";
-    return 0;
+    return 0; // ----- >>
   }
+
+  // Read points from file
   std::string filegraph   = argv[1];
-
   std::list<Point> lp;
-
-  //read input
   std::ifstream is(filegraph.c_str());
   int n;
   is >> n;
@@ -45,51 +51,45 @@ int main (int argc, char * const argv[])
     lp.push_back(p);
   }
 
-  // compute alpha shape
+  // alpha shape construction from points
   Alpha_shape_3 as(lp.begin(),lp.end());
   std::cout << "Alpha shape computed in REGULARIZED mode by default" << std::endl;
 
-  std::vector<CGAL::Object> the_objects;
-  std::vector<Alpha_shape_3::FT> the_ft;
+  // filtration with alpha values from alpha shape
+  std::vector<Object> the_objects;
+  std::vector<Alpha_value_type> the_alpha_values;
 
-  typedef CGAL::Dispatch_output_iterator<
-    CGAL::cpp11::tuple<CGAL::Object, Alpha_shape_3::FT>,
-    CGAL::cpp11::tuple<std::back_insert_iterator< std::vector<CGAL::Object> >,
-                       std::back_insert_iterator< std::vector<Alpha_shape_3::FT> >
-                       > > Dispatch;
-
-  Dispatch disp = CGAL::dispatch_output<CGAL::Object, Alpha_shape_3::FT>(
-    std::back_inserter(the_objects),
-    std::back_inserter(the_ft));
+  Dispatch disp = CGAL::dispatch_output<Object, Alpha_value_type>( std::back_inserter(the_objects), std::back_inserter(the_alpha_values));
 
   as.filtration_with_alpha_values(disp);
   std::cout << "filtration_with_alpha_values returns : " << the_objects.size() << " objects" << std::endl;
-  std::cout << "filtration_with_alpha_values returns : " << the_ft.size() << " FT" << std::endl;
 
-  typename Alpha_shape_3::Cell_handle cell;
-  typename Alpha_shape_3::Facet      facet;
-  typename Alpha_shape_3::Edge       edge;
-  typename Alpha_shape_3::Vertex_handle  vertex;
+  Alpha_shape_3::size_type count_vertices = 0;
+  Alpha_shape_3::size_type count_edges    = 0;
+  Alpha_shape_3::size_type count_facets   = 0;
+  Alpha_shape_3::size_type count_cells    = 0;
 
-  typename Alpha_shape_3::size_type count_vertices = 0;
-  typename Alpha_shape_3::size_type count_edges = 0;
-  typename Alpha_shape_3::size_type count_facets = 0;
-  typename Alpha_shape_3::size_type count_cells = 0;
-
+  // Loop on objects vector
   for(auto object_iterator: the_objects)
   {
-	  if(CGAL::assign(cell,object_iterator))
+	  if (const Cell_handle* cell = CGAL::object_cast<Cell_handle>(&object_iterator))
+	  {
+		  std::cout << "Cell[" << (*cell)->vertex(0)->point() << ";" << (*cell)->vertex(1)->point() << ";" << (*cell)->vertex(2)->point() << ";" << (*cell)->vertex(3)->point() << "]" << std::endl;
 		  count_cells++;
-		  //std::cout << "intersection object is a cell=" << std::endl;
-	  if(CGAL::assign(facet,object_iterator))
+	  }
+	  if (const Facet* facet = CGAL::object_cast<Facet>(&object_iterator))
+	  {
 		  count_facets++;
-		  //std::cout << "intersection object is a facet=" << std::endl;
-	  if(CGAL::assign(edge,object_iterator))
+	  }
+	  if (const Edge* edge = CGAL::object_cast<Edge>(&object_iterator))
+	  {
 		  count_edges++;
-		  //std::cout << "intersection object is a edge=" << std::endl;
-	  if(CGAL::assign(vertex,object_iterator))
+	  }
+	  if (const Vertex_handle* vertex = CGAL::object_cast<Vertex_handle>(&object_iterator))
+	  {
 		  count_vertices++;
-		  //std::cout << "intersection object is a edge=" << std::endl;
+		  std::cout << "intersection object is a vertex=" << (*vertex)->point() << std::endl;
+	  }
 
   }
   std::cout << "vertices \t\t" << count_vertices << std::endl;
@@ -97,10 +97,10 @@ int main (int argc, char * const argv[])
   std::cout << "facets \t\t"   << count_facets << std::endl;
   std::cout << "cells \t\t"    << count_cells << std::endl;
 
-  /*for(auto ft_iterator: the_ft)
+  /*for(auto alpha_value_iterator: the_alpha_values)
   {
-    if (ft_iterator < *opt)
-	  std::cout << "Approx=" << ft_iterator << std::endl;
+    if (alpha_value_iterator < *opt)
+	  std::cout << "Approx=" << alpha_value_iterator << std::endl;
   }*/
 
 
