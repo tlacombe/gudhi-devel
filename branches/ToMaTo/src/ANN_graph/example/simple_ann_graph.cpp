ba3cee841d847b1f0c5719a8daa2fc8249050ff7
vrouvrea <vrouvrea@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2015-09-30 06:53:25 +0000

    Missing comments and mu instead of radius
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi@809 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 05bffdd62fffc01d963383edd92803e8d08a75d5

--- branches/ToMaTo/src/ANN_graph/example/simple_ann_graph.cpp
+++ branches/ToMaTo/src/ANN_graph/example/simple_ann_graph.cpp
@@ -27,27 +27,30 @@ int main(int argc, char *argv[]) {
   point_cloud.push_back(ANN_point(point_coord, point_dimension));
   
   // create distance structure
-  double radius_1 = 1e20;
-  ANN_ngbh_graph ngbh_graph(point_cloud.begin(), point_cloud.end(), point_dimension, radius_1);
+  double mu = 1e20;
+  ANN_ngbh_graph ngbh_graph(point_cloud.begin(), point_cloud.end(), point_dimension, mu);
   
   for (auto point_iterator = point_cloud.begin(); point_iterator < point_cloud.end(); point_iterator++) {
     vector<Iterator> vi;
+    // get_neighbors inits a vector of iterator on neighbors points except the given point
     ngbh_graph.get_neighbors(point_iterator, vi);
     cout << "Point [" << point_iterator->first_3_cordinates() << "] - number of neighbors in a ball of radius ";
-    cout << radius_1 << " = " << vi.size() << endl;
+    cout << mu << " = " << vi.size() << endl;
   }
   cout << endl;
 
-  double radius_2 = 0.5;
+  double radius_1 = 0.5;
   for (auto point_iterator = point_cloud.begin(); point_iterator < point_cloud.end(); point_iterator++) {
+    // get_num_neighbors returns the number of neighbors including the given point
     cout << "Point [" << point_iterator->first_3_cordinates() << "] - has ";
-    cout << ngbh_graph.get_num_neighbors(point_iterator, radius_2) << " neighbors in a ball of radius " << radius_2 << endl;
+    cout << ngbh_graph.get_num_neighbors(point_iterator, radius_1) << " neighbors in a ball of radius " << radius_1 << endl;
   }
   cout << endl;
   
   int k = 3;
   for (auto point_iterator = point_cloud.begin(); point_iterator < point_cloud.end(); point_iterator++) {
     double* ndist = new double[k];
+    // get_neighbors_dist inits an array of distance with k-neighbors points including the given point
     ngbh_graph.get_neighbors_dist(point_iterator, k, ndist);
     cout << "Point [" << point_iterator->first_3_cordinates() << "] - " << k << "-closest neighbors distances are:";
     for (int i = 0; i < k; i++)
@@ -57,12 +60,14 @@ int main(int argc, char *argv[]) {
   }
   cout << endl;
   
-  double radius_3 = 1.0;
+  double radius_2 = 1.0;
   for (auto point_iterator = point_cloud.begin(); point_iterator < point_cloud.end(); point_iterator++) {
     double* ndist = new double[point_cloud.size()];
-    int nb_points = ngbh_graph.get_neighbors_dist_r(point_iterator, radius_3, ndist);
+    // get_neighbors_dist_r inits an array of distance with neighbors points in a ball of a given radius including the
+    // given point
+    int nb_points = ngbh_graph.get_neighbors_dist_r(point_iterator, radius_2, ndist);
     cout << "Point [" << point_iterator->first_3_cordinates() << "] - closest neighbors, in a ball of radius ";
-    cout << radius_3 << ", distances are:";
+    cout << radius_2 << ", distances are:";
     for (int i = 0; i < nb_points; i++)
       cout << " " << ndist[i];
     cout << endl;
