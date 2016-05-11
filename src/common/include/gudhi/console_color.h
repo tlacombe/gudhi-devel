72c6aa82639b662b25aff9b93e588890288fa4c8
cjamin <cjamin@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-05-11 15:44:41 +0000

    Move console_color.h
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/tangentialcomplex@1173 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: efe747a60b8c85ebe56ff4dd3dc9cfa186afdb1a

--- src/common/include/gudhi/console_color.h
+++ src/common/include/gudhi/console_color.h
@@ -1,5 +1,27 @@
-#ifndef CONSOLE_COLOR_H_
-#define CONSOLE_COLOR_H_
+/*    This file is part of the Gudhi Library. The Gudhi library
+ *    (Geometric Understanding in Higher Dimensions) is a generic C++
+ *    library for computational topology.
+ *
+ *    Author(s):       Clement Jamin
+ *
+ *    Copyright (C) 2016  INRIA Sophia-Antipolis (France)
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#ifndef GUDHI_CONSOLE_COLOR_H_
+#define GUDHI_CONSOLE_COLOR_H_
 
 #include <iostream>
 
