90f65c59f76d6ae833e3b5cf3640c9be3a74edd5
skachano <skachano@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-05-25 10:22:11 +0000

    Test now features points instead of ints
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/tangentialcomplex@1197 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: c2b0372e0b6e47ad22f5356468dfd776371b1c3b

--- src/Subsampling/test/landmarking.cpp
+++ src/Subsampling/test/landmarking.cpp
@@ -6,9 +6,32 @@
 #include <vector>
 #include <iterator>
 
+#include <CGAL/Epick_d.h>
+
+
 int main() {
-  typedef int Point_d;
-  std::vector<Point_d> vect = {0,1,2,3,4,5,6,7,8,9,10};
+  typedef CGAL::Epick_d<CGAL::Dynamic_dimension_tag>                K;
+  typedef typename K::FT                                            FT;
+  typedef typename K::Point_d                                       Point_d;
+  
+  std::vector<Point_d> vect;
+  vect.push_back(Point_d(std::vector<FT>({0,0,0,0})));
+  vect.push_back(Point_d(std::vector<FT>({0,0,0,1})));
+  vect.push_back(Point_d(std::vector<FT>({0,0,1,0})));
+  vect.push_back(Point_d(std::vector<FT>({0,0,1,1})));
+  vect.push_back(Point_d(std::vector<FT>({0,1,0,1})));
+  vect.push_back(Point_d(std::vector<FT>({0,1,1,0})));
+  vect.push_back(Point_d(std::vector<FT>({0,1,1,1})));
+  vect.push_back(Point_d(std::vector<FT>({1,0,0,0})));
+  vect.push_back(Point_d(std::vector<FT>({1,0,0,1})));
+  vect.push_back(Point_d(std::vector<FT>({1,0,1,0})));
+  vect.push_back(Point_d(std::vector<FT>({1,0,1,1})));
+  vect.push_back(Point_d(std::vector<FT>({1,1,0,0})));
+  vect.push_back(Point_d(std::vector<FT>({1,1,0,1})));
+  vect.push_back(Point_d(std::vector<FT>({1,1,1,0})));
+  vect.push_back(Point_d(std::vector<FT>({1,1,1,1})));
+  
+  
   std::vector<Point_d> landmarks;
   Gudhi::landmark_choice_by_random_point(vect, 5, std::back_inserter(landmarks));
   std::cout << "landmark vector contains: ";
