bdbc71111bbf3a78dc6ace8e934340d76989d14f
skachano <skachano@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-05-30 09:20:09 +0000

    Landmark choice by farthest point (no improvement)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/tangentialcomplex@1218 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: b6ef1d15abf3c1eb0bdd3b0ecbfc26804705bfcd

--- src/Subsampling/test/landmarking.cpp
+++ src/Subsampling/test/landmarking.cpp
@@ -1,8 +1,9 @@
-#ifdef _DEBUG
-# define TBB_USE_THREADING_TOOL
-#endif
+// #ifdef _DEBUG
+// # define TBB_USE_THREADING_TOOL
+// #endif
 
 #include <gudhi/Landmark_choice_by_random_point.h>
+#include <gudhi/Landmark_choice_by_farthest_point.h>
 #include <vector>
 #include <iterator>
 
@@ -19,6 +20,7 @@ int main() {
   vect.push_back(Point_d(std::vector<FT>({0,0,0,1})));
   vect.push_back(Point_d(std::vector<FT>({0,0,1,0})));
   vect.push_back(Point_d(std::vector<FT>({0,0,1,1})));
+  vect.push_back(Point_d(std::vector<FT>({0,1,0,0})));
   vect.push_back(Point_d(std::vector<FT>({0,1,0,1})));
   vect.push_back(Point_d(std::vector<FT>({0,1,1,0})));
   vect.push_back(Point_d(std::vector<FT>({0,1,1,1})));
@@ -36,6 +38,10 @@ int main() {
   Gudhi::landmark_choice_by_random_point(vect, 5, std::back_inserter(landmarks));
   std::cout << "landmark vector contains: ";
   for (auto l: landmarks)
-    std::cout << l << " ";
-  std::cout << "\n";
+    std::cout << l << "\n";
+
+  landmarks.clear();
+  K k;
+  Gudhi::landmark_choice_by_farthest_point(k, vect, 16, std::back_inserter(landmarks));
+  
 }
