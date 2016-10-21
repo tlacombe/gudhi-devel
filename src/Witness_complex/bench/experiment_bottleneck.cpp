3443faf39c6a71f02f957181053538b03e2d660b
skachano <skachano@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2016-10-21 09:01:39 +0000

    Commited experiment
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/relaxed-witness-experiments@1740 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 22ad40a553da224add748df723dc8e5a266d07c5

--- src/Witness_complex/bench/experiment_bottleneck.cpp
+++ src/Witness_complex/bench/experiment_bottleneck.cpp
@@ -6,6 +6,9 @@
 #include <gudhi/pick_n_random_points.h>
 #include <gudhi/Points_off_io.h>
 
+#include <gudhi/Persistent_cohomology.h>
+#include <gudhi/Graph_matching.h>
+
 #include <CGAL/Epick_d.h>
 
 #include <iostream>
@@ -13,6 +16,9 @@
 #include <ctime>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <stdio.h>
+#include <time.h>
 
 typedef CGAL::Epick_d<CGAL::Dynamic_dimension_tag> K;
 typedef typename K::Point_d Point_d;
@@ -20,8 +26,58 @@ typedef typename Gudhi::witness_complex::Witness_complex<K> Witness_complex;
 typedef std::vector< Vertex_handle > typeVectorVertex;
 typedef std::vector< Point_d > Point_vector;
 
+typedef Gudhi::Simplex_tree<> SimplexTree;
+typedef typename Gudhi::persistent_cohomology::Persistent_cohomology<SimplexTree, Gudhi::persistent_cohomology::Field_Zp> PersistentCohomology;
+typedef std::vector<std::pair<double, double> > GraphTable;
+
+GraphTable read_diagram_from_file( const char* filename )
+{
+    std::ifstream in;
+    in.open( filename );
+    std::vector< std::pair<double, double> > result;
+    if ( !in.is_open() )
+    {
+        std::cerr << "File : " << filename << " do not exist. The program will now terminate \n";
+        throw "File do not exist \n";
+    }
+
+    std::string line;
+    while (!in.eof())
+    {
+        getline(in,line);
+        if ( line.length() != 0 )
+        {
+            std::stringstream lineSS;
+            lineSS << line;
+            double beginn, endd;
+            lineSS >> beginn;
+            lineSS >> endd;
+            result.push_back( std::make_pair( beginn , endd ) );
+        }
+    }
+    in.close();
+    return result;
+}  //read_diagram_from_file
+
+
+void write_graph(SimplexTree& simplextree, PersistentCohomology& pers, GraphTable& graph)
+{
+  std::time_t raw_time;
+  raw_time = time(NULL);
+  char* filename;
+  sprintf(filename, "temp_%s", asctime(localtime(&raw_time)));
+  //std::ofstream f_out(filename);
+  //graph = read_diagram_from_file(filename);
+  for (auto pair: pers.get_persistent_pairs())
+    if (simplextree.dimension(std::get<0>(pair)) == 1) {
+      double alpha1 = simplextree.filtration(std::get<0>(pair));
+      double alpha2 = simplextree.filtration(std::get<1>(pair));
+      graph.push_back(std::make_pair(alpha1,alpha2));
+    }
+}
+
 int main(int argc, char * const argv[]) {
-  if (argc != 4) {
+  if (argc != 5) {
     std::cerr << "Usage: " << argv[0]
         << " path_to_point_file number_of_landmarks max_squared_alpha limit_dimension\n";
     return 0;
@@ -31,10 +87,10 @@ int main(int argc, char * const argv[]) {
   int nbL = atoi(argv[2]), lim_dim = atoi(argv[4]);
   double alpha2 = atof(argv[3]);
   clock_t start, end;
-  Gudhi::Simplex_tree<> simplex_tree;
+  SimplexTree simplex_tree1, simplex_tree2;
 
   // Read the point file
-  Point_vector point_vector, landmarks;
+  Point_vector point_vector, landmarks1, landmarks2;
   Gudhi::Points_off_reader<Point_d> off_reader(file_name);
   if (!off_reader.is_valid()) {
       std::cerr << "Witness complex - Unable to read file " << file_name << "\n";
@@ -46,16 +102,36 @@ int main(int argc, char * const argv[]) {
   std::cout << "Ambient dimension is " << point_vector[0].dimension() << ".\n";
 
   // Choose landmarks
-  Gudhi::subsampling::pick_n_random_points(point_vector, nbL, std::back_inserter(landmarks));
-
+  Gudhi::subsampling::pick_n_random_points(point_vector, nbL, std::back_inserter(landmarks1));
+  Gudhi::subsampling::pick_n_random_points(point_vector, nbL, std::back_inserter(landmarks2));
+  
   // Compute witness complex
   start = clock();
-  Witness_complex witness_complex(landmarks,
-                                  point_vector);
+  Witness_complex witness_complex1(landmarks1,
+                                   point_vector);
 
-  witness_complex.create_complex(simplex_tree, alpha2, lim_dim);
+  witness_complex1.create_complex(simplex_tree1, alpha2, lim_dim);
   end = clock();
-  std::cout << "Witness complex took "
+  std::cout << "Witness complex 1 took "
       << static_cast<double>(end - start) / CLOCKS_PER_SEC << " s. \n";
-  std::cout << "Number of simplices is: " << simplex_tree.num_simplices() << "\n";
+  std::cout << "Number of simplices is: " << simplex_tree1.num_simplices() << "\n";
+
+  start = clock();
+  Witness_complex witness_complex2(landmarks2,
+                                   point_vector);
+
+  witness_complex2.create_complex(simplex_tree2, alpha2, lim_dim);
+  end = clock();
+  std::cout << "Witness complex 2 took "
+      << static_cast<double>(end - start) / CLOCKS_PER_SEC << " s. \n";
+  std::cout << "Number of simplices is: " << simplex_tree2.num_simplices() << "\n";
+
+  PersistentCohomology pers1(simplex_tree1), pers2(simplex_tree2);
+  GraphTable graph1, graph2;
+  write_graph(simplex_tree1, pers1, graph1);
+  write_graph(simplex_tree2, pers2, graph2);
+
+  double bd = Gudhi::bottleneck::bottleneck_distance(graph1, graph2);
+  std::cout << "The bottleneck distance is: " << bd << "\n";
+  
 }
