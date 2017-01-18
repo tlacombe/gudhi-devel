79a66f8de237bddc7193e3ab6da78426c80ed557
cmaria <cmaria@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-01-18 14:08:44 +0000

    Nodes with hooks and proper copy constructor (moving hooks)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/fast_cofaces_ST@1953 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 486bc9e797ad340e7bccbc63ebe6aefc46d0c595

--- src/Simplex_tree/include/gudhi/Simplex_tree/Simplex_tree_node_explicit_storage.h
+++ src/Simplex_tree/include/gudhi/Simplex_tree/Simplex_tree_node_explicit_storage.h
@@ -49,7 +49,15 @@ struct Simplex_tree_node_explicit_storage : SimplexTree::Filtration_simplex_base
       : children_(sib) {
     this->assign_filtration(filtration);
   }
-
+//If hooks are stored, they will be exchanged by the copy constructor
+//and the hooks of *this will become unvalid.
+  Simplex_tree_node_explicit_storage(
+     const Simplex_tree_node_explicit_storage &other) 
+  : SimplexTree::Filtration_simplex_base(other) 
+  , SimplexTree::Key_simplex_base(other) 
+  , SimplexTree::Hooks_simplex_base(other)
+  , SimplexTree::Annotation_simplex_base(other)
+  , children_(other.children_) {}
   /*
    * Assign children to the node
    */
