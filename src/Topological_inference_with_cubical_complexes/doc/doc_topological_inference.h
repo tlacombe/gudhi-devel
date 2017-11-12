29cf5c5122cf961350d672b0e79b5a970ad50e83
pdlotko <pdlotko@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-11-12 16:49:27 +0000

    problem with doc.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/topological_inference_with_cubical_complexes@2866 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: fd343d3728ac00e0e79df2e599f65b9e5d1a55f2

--- src/Topological_inference_with_cubical_complexes/doc/doc_topological_inference.h
+++ src/Topological_inference_with_cubical_complexes/doc/doc_topological_inference.h
@@ -29,6 +29,7 @@ namespace Gudhi {
 namespace Topological_inference_with_cubical_complexes {
 
 /**  \defgroup Topological_inference_with_cubical_complexes Topological_inference_with_cubical_complexes
+ *   \ingroup Topological_inference_with_cubical_complexes
  * 
  * \author    Pawel Dlotko
  * @{
