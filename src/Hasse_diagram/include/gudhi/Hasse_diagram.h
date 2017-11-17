591b3c67b6ffb177bef392570159824145aad58b
pdlotko <pdlotko@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2017-11-17 15:39:36 +0000

    Further developement
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/hasse_diagram@2910 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: c42a7f48f613fcafac2eab9a81207669a1f5fdf6

--- src/Hasse_diagram/include/gudhi/Hasse_diagram.h
+++ src/Hasse_diagram/include/gudhi/Hasse_diagram.h
@@ -38,10 +38,14 @@ namespace Hasse_diagram {
 	
 template < typename Cell_type > class Hasse_diagram;
 
-template <typename Incidence_type, typename Additional_information = void>
+template <typename Incidence_type_, typename Filtration_type_ , typename Additional_information_ = void>
 class Cell
 {
 public:
+	typedef Incidence_type_ Incidence_type;
+	typedef Filtration_type_ Filtration_type;
+	typedef Additional_information_ Additional_information;
+	
     /**
      * TODO
     **/  
@@ -107,7 +111,7 @@ public:
 	/**
 	 * TODO
 	**/ 	
-	bool& deleted(){return this->deleted_;}
+	bool deleted(){return this->deleted_;}
 	
 	template < typename Cell_type >
 	friend class Hasse_diagram;
@@ -140,6 +144,22 @@ public:
 		}
 		this->coBoundary.swap( new_coBoundary );
 	}
+	
+	/**
+	 * Writing to a stream operator.
+	**/ 	
+	friend std::ostream operator<<( std::ostream& out, Cell<Incidence_type,Filtration_type,Additional_information> const& c )
+	{
+		 out << c.dimension << " " << c.filtration << std::endl;
+		 for ( size_t bd = 0 ; bd != c.boundary.size() ; ++bd )
+	     {
+			 //do not write out the cells that has been deleted
+			 if ( c.boundary[bd].first->deleted() )continue;
+			 out << c.boundary[bd].first->position << " " << c.boundary[bd].second << " ";
+		 }
+		 out << std::endl;
+		return out;
+	}
 private:	
 	std::vector< std::pair<Cell*,int> > boundary;
 	std::vector< std::pair<Cell*,int> > coBoundary;
@@ -147,6 +167,15 @@ private:
 	Additional_information additional_info;	
 	size_t position;
 	bool deleted_; //for lazy delete	
+	Filtration_type filtration;
+	
+	/**
+	 * A procedure to delete a cell. It is a private function of the Cell
+	 * class, since in the Hasse_diagram class I want to have a control 
+	 * of removal of cells. Therefore, to remove cell please use
+	 * remove_cell in the Hasse_diagram structure.
+	**/ 
+	void delete_cell(){ this->deleted_ = true; }
 };//Cell
 
 
@@ -155,24 +184,34 @@ class Hasse_diagram
 {
 public:
 	/**
-	 * TODO
+	 * Default constructor.
 	**/ 
 	Hasse_diagram(){};
 	
 	/**
-	 * TODO
+	 * Creating Hasse diagram from a file. The file format is the following:
+	 * Number of cells
+	 * cell dimension
+	 * ids of cell boundary elements followed by the incidence coefficient.
+	 * the two lines above are repeated for each cell. 
+	 * It is assumed that the id of a cell is its position in the file. 	 
 	**/ 	
     Hasse_diagram( const char* filename );
     
     /**
-	 * TODO
+	 * Constructor to create a Hasse diagram from a vector of cells. It is assumed 
+	 * that all the cells have boundaries set up. Setting up the coboundaries will
+	 * be done in the constructor based on the information about boundaries. 
 	**/ 
-    Hasse_diagram( const std::vector< Cell_type* >& cells_ ):cells(cells_),number_of_deleted_cells(0){};
+    Hasse_diagram( const std::vector< Cell_type* >& cells_ ):cells(cells_),number_of_deleted_cells(0)
+    {
+		this->set_up_coboundaries();
+	};
 
 
 	//those iterators should skip the deleted cells. 
 	/**
-	 * TODO
+	 * aaa, those iterators should skip the deleted elements.
 	**/ 
 	typedef typename std::vector< size_t >::const_iterator All_cells_iterator;
     typedef typename std::vector< size_t > All_cells_iterator_range;
@@ -221,17 +260,157 @@ public:
 		}
 		this->cells.swap(new_cells);
 	} 
+	
+	/**
+	 * Procedure that allow to add a cell into the structure.
+	**/ 
+	void add_cell( Cell_type* cell )
+	{
+		cell->position = this->cells.size();
+		this->cells.push_back( cell );		
+	}
+	
+	/**
+	 * Procedure that allow to remove a cell into the structure.
+	**/ 
+	void remove_cell( Cell_type* cell )
+	{
+		cell->delete_cell();
+		this->number_of_deleted_cells++;
+		//in case the structure gets too fragmented, we are calling the 
+		//to clean it up.
+		if ( this->number_of_deleted_cells/(double)(this->cells.size) > 
+			this->percentate_of_removed_cells_that_triggers_reorganization_of_structure )
+		{
+			this->clean_up_the_structure();
+		}
+	}
+	
+	/**
+	 * A procedure writng Hasse diagram to file. The Hasse diagram can be later
+	 * reconstructed using Hasse_diagram( const char* filename ) constructor.
+	**/ 
+	void write_to_file( const char* filename );
+	
+	/**
+	 * Writing to a stream operator.
+	**/ 	
+	friend  std::ostream operator<<( std::ostream& out, const Hasse_diagram< Cell_type >& c )
+	{
+		for ( size_t i = 0 ; i != c.cells.size() ; ++i )
+		{
+			//if the cell is deleted, ignore it.
+			if ( c.cells[i]->deleted() )continue;
+			out << c.cells[i];
+		}
+		return out;
+	}
+	
+	//From here on we have implementation of methods that are required to use
+	//this class with persistent homology engine.
+		
+	typedef typename Cell_type::Filtration_type Filtration_value;
+    typedef unsigned Simplex_key;
+    typedef Simplex_key Simplex_handle;
+    
+    size_t num_simplices() 
+    {
+		return this->cells.size();
+    }
+    
+    //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA    
+    //Filtration_simplex_range filtration_simplex_range()
+    //{
+		//TODO		
+	//}
+	
+	Simplex_key key(Simplex_handle sh) 
+	{
+		return sh;
+	}
+
+	Simplex_key null_key() 
+	{
+		return std::numeric_limits<unsigned>::infinity();
+	}
+
+	Simplex_handle simplex(Simplex_key key) 
+	{
+		return key;
+	}
+
+	Simplex_handle null_simplex() 
+	{
+		return std::numeric_limits<unsigned>::infinity();
+	}
+
+	Filtration_value filtration(Simplex_handle sh) 
+	{
+		if (sh == null_simplex()) 
+		{
+		  return std::numeric_limits<Filtration_value>::infinity();
+		}
+		return this->cells[ sh ]->filtration;
+	}
+
+	int dimension(Simplex_handle sh) 
+	{
+		if (sh == null_simplex()) 
+		{
+		  return std::numeric_limits<Filtration_value>::infinity();
+		}
+		return this->cells[ sh ]->dimension;
+	}
+
+	int dimension() 
+	{
+		int top_dimension = 0;
+		for ( size_t i = 0 ; i != this->cells.size() ; ++i )
+		{
+			if ( top_dimension < this->cells[i]->dimension )
+			{
+				top_dimension = this->cells[i]->dimension;
+			}
+		}
+		return top_dimension;
+	}
+
+	std::pair<Simplex_handle, Simplex_handle> endpoints(Simplex_handle sh) 
+	{
+		return std::pair<Simplex_handle, Simplex_handle>(
+							this->cells[sh].boundary[0].first->position, 
+							this->cells[sh].boundary[1].first->position
+														);
+	}
+
+	void assign_key(Simplex_handle sh, Simplex_key key) 
+	{
+		//TODO
+		//complex_[sh].key_ = key;
+	}
+
+	//Boundary_simplex_range boundary_simplex_range(Simplex_handle sh) 
+	//{
+		//TODO
+		//return Boundary_simplex_range(complex_[sh].boundary_.begin(), complex_[sh].boundary_.end());
+	//}	
 private:	
 	std::vector< Cell_type* > cells;
 	
-	size_t number_of_deleted_cells; //to check how fragmented the data structure is (as a result of removing cells). Can we even use it if deleting is happening on the level of cells??
+	//to check how fragmented the data structure is (as a result of removing cells).
+	size_t number_of_deleted_cells; 
 	
 	/**
 	 * This procedure assumes that the boundaries are already set up for all
 	 * the cells, and set up the coboundaries based on them.
 	**/  
 	void set_up_coboundaries();
+	
+	static double percentate_of_removed_cells_that_triggers_reorganization_of_structure;
 };//Hasse_diagram
+
+template <typename Cell_type>
+double Hasse_diagram<Cell_type>::percentate_of_removed_cells_that_triggers_reorganization_of_structure = 0.8;
 
 
 template < typename Cell_type >
@@ -244,8 +423,8 @@ Hasse_diagram<Cell_type>::Hasse_diagram( const char* filename )
 	//has already been constructed. 
 	//Here is the format of a file:
 	//Number of cells
-	//cell id cell dimension
-	//ids of cell boundary elements follwoed by the incdence coefficient.
+	//cell dimension
+	//ids of cell boundary elements followed by the incidence coefficient.
 	//Note that coboundary vector will be computed based on boundary vector.
 	bool dbg = true;
 	
@@ -266,22 +445,35 @@ Hasse_diagram<Cell_type>::Hasse_diagram( const char* filename )
 	{
 		Cell_type* new_cell = new Cell_type();
 		new_cell->position = i;
-		in >> new_cell->dimension;
 		std::string line;
 		std::getline(in, line);
 		std::istringstream iss(line);
+		iss >> new_cell->dimension;
+		if ( iss.good() )
+		{
+			//in this case we still have a filtration value to be read
+			//from the file.
+			iss >> new_cell->filtration;
+		}
+		else
+		{
+			new_cell->filtration = 0;
+		}
+		
+		std::getline(in, line);		
+		std::istringstream iss1(line);
 		unsigned cell_id;
 		int incidence_coef;
 		std::vector< std::pair< unsigned,int > > bdry;
 		bdry.reserve( size_of_last_boundary );
-		while ( iss.good() )
+		while ( iss1.good() )
 		{
-			iss >> cell_id;
+			iss1 >> cell_id;
 			if ( cell_id >= i )
 			{
 				std::cerr << "Wrong format of a file. THe cell number : " << i << " contain in a boundary a cell that has not been introduced yet.\n";
 			}
-			iss >> incidence_coef;			
+			iss1 >> incidence_coef;			
 			bdry.push_back( std::pair< unsigned,int >(cell_id,incidence_coef) );
 		}
 		size_of_last_boundary = bdry.size();
@@ -292,10 +484,10 @@ Hasse_diagram<Cell_type>::Hasse_diagram( const char* filename )
 		}		
 		this->cells.push_back( new_cell );
 	}
-	//now once the boundaries are set, we are to set up the coboundaries.
-	
+	//now once the boundaries are set, we are to set up the coboundaries.	
 	this->set_up_coboundaries();
 }
+
 
 template < typename Cell_type >
 void Hasse_diagram<Cell_type>::set_up_coboundaries()
@@ -322,11 +514,37 @@ void Hasse_diagram<Cell_type>::set_up_coboundaries()
 	for ( size_t i = 0 ; i != number_of_cells ; ++i )
 	{
 		for ( size_t bd = 0 ; bd != this->cells[i]->boundary.size() ; ++bd )
-		{
-			this->cells[i]->boundary[bd]->coBoundary.push_back( this->cells[i] );
+		{			
+			this->cells[ this->cells[i]->boundary[bd].first->position ]
+			->
+			coBoundary.push_back
+			                    ( 
+			                    std::make_pair(this->cells[i], 
+								this->cells[i]->boundary[bd].second)
+			                    );
 		}
 	}
 }
+
+template < typename Cell_type >
+void Hasse_diagram<Cell_type>::write_to_file( const char* filename )
+{
+	std::ofstream out( filename );
+	//If there are any deleted cells, then we need to clean up the structure 
+	//first before writing it to a file. The reason for that is because the 
+	//file format assumes a continuous enumeration of cells (from zero to the
+	//number od cells). This is not satisfied if we have in the structure any
+	//deleted elements.	
+	if ( this->number_of_deleted_cells != 0 )
+	{
+		this->clean_up_the_structure();
+	}
+	//now we can write to a file the number of (non deleted) cells in the structure. 
+	out << this->cells.size() << std::endl;
+	//and then the rest of the Hasse diagram.
+	out << *this;		
+	out.close();
+}//template < typename Cell_type >
 
 
 }//namespace Hasse_diagram
