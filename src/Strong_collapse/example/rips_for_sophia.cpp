679fda1b915853928e219a38c83b24665d912afe
spritam <spritam@636b058d-ea47-450e-bf9e-a15bfbe3eedb>
2018-02-12 13:02:25 +0000

    Updating
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/gudhi/branches/Strong_Collapse@3238 636b058d-ea47-450e-bf9e-a15bfbe3eedb
    
    Former-commit-id: 55c4887d9d9cd6ac5ff79ee80b9e884d8cb2b093

--- src/Strong_collapse/example/rips_for_sophia.cpp
+++ src/Strong_collapse/example/rips_for_sophia.cpp
@@ -5,48 +5,52 @@
 #include <CGAL/Epick_d.h>
 #include <CGAL/point_generators_d.h>
 #include <CGAL/Random.h>
-#include <cmath>
 
+#include <gudhi/PointSetGen.h>
+
+#include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <limits>
 #include <algorithm>
 
-using Point = CGAL::Epick_d< CGAL::Dimension_tag<10> >::Point_d;
-using Fake_simplex_tree     = Gudhi::Fake_simplex_tree ;
-
-using Filtration_value = Fake_simplex_tree::Filtration_value;
-using Rips_complex     = Gudhi::rips_complex::Rips_complex<Filtration_value>;
+using Fake_simplex_tree   = Gudhi::Fake_simplex_tree ;
+using Filtration_value    = Fake_simplex_tree::Filtration_value;
+using Rips_complex        = Gudhi::rips_complex::Rips_complex<Filtration_value>;
 
 using Vector_of_points = std::vector<Point>;
-
-void generate_points_sphere(Vector_of_points& W, int nbP, int dim) {
-	CGAL::Random_points_on_sphere_d<Point> rp(dim, 1);
-	for (int i = 0; i < nbP; i++)
-		W.push_back(*rp++);
-}
 
 int main() {
 	
     Vector_of_points points;
     Map map_empty;
 
-    std::size_t num_pts = 100;
+    std::size_t init_num_pts = 50;
     int maxSplxDim = 10;
-    int sphereDim = 2;
-	
+    int sphereDim = 2;          // ( actual dimension is sphereDim -1)
+    int radius    = 1;
+    int multiplier_steps = 2;
+    int num_spheres = 4;
+    int tot_num_points = init_num_pts*(pow(multiplier_steps, num_spheres) - 1);
+
     Fake_simplex_tree *subComplex ;   
     SparseMsMatrix * mat_coll       = new SparseMsMatrix(); 
-    SparseMsMatrix * mat_prev_coll  = new SparseMsMatrix(num_pts, 100*num_pts); 
+    SparseMsMatrix * mat_prev_coll  = new SparseMsMatrix(tot_num_points, 100*tot_num_points); 
 
-    FormatTower towerFormater(num_pts) ;
-    FormatTower origTowerFormater(num_pts);
-    generate_points_sphere(points,num_pts,sphereDim); // 2 for S2
-	
+    FormatTower towerFormater(tot_num_points) ;
+    FormatTower origTowerFormater(tot_num_points);
+    PointSetGen point_generator;
+    // point_generator.generate_points_sphere(points,tot_num_points,sphereDim,radius); // 2 for S2
+   
+    // point_generator.generate_points_wedged_sphere(points, init_num_pts, sphereDim, radius, multiplier_steps, num_spheres);
+    point_generator.generate_points_concentric_sphere(points, init_num_pts, sphereDim, radius, multiplier_steps, num_spheres);
+    std::cout << "Point Set Generated: "  <<std::endl;
+    // for(int i = 0; i <tot_num_points; i++ )
+    //     point_generator.print_point(points.at(i), sphereDim);
+
     int numSimComp = 15;
-    // double threshold_vals[3] = {0.1,0.2,0.4};// {0.25,0.5,0.75,1,1.1};
-    double threshold_vals = 0.6;
+    double threshold_vals = 0.3;
     double incremental_step = 0.01;
      
     for(int i = 0 ; i < numSimComp ; ++i)
@@ -55,16 +59,14 @@ int main() {
 
         Rips_complex rips_complex_from_points(points, threshold_vals, Gudhi::Euclidean_distance());
         rips_complex_from_points.create_complex(*subComplex, maxSplxDim);
-        std::cout << "Simplex tree for parameter " << threshold_vals << " created." <<std::endl;
+        std::cout << "Rips complex for parameter " << threshold_vals << " created." <<std::endl;
         
         mat_coll = new SparseMsMatrix(*subComplex);
 
         if( i == numSimComp-1)
         {
-            origTowerFormater.one_step_tower(* new SparseMsMatrix(num_pts, 100*num_pts), *mat_coll, map_empty, "OriginalTowerRips.txt");
+            origTowerFormater.one_step_tower(* new SparseMsMatrix(tot_num_points, 100*tot_num_points), *mat_coll, map_empty, "OriginalTowerRips.txt");
             std::cout << "Original Tower computed" << std::endl;
-            // delete st_prev;
-            // st_prev = new Fake_simplex_tree(subComplex);
         }
 
         mat_coll->strong_collapse();
--- src/Strong_collapse/example/sparseCollapseRips.cpp
+++ src/Strong_collapse/example/sparseCollapseRips.cpp
@@ -1,5 +1,4 @@
 #include <gudhi/SparseMsMatrix.h>
-#include <gudhi/Fake_simplex_tree.h>
 #include <gudhi/Simplex_tree.h>
 
 #include <gudhi/Rips_complex.h>
@@ -13,6 +12,7 @@
 #include <CGAL/Random.h>
 
 #include <boost/program_options.hpp>
+#include <gudhi/PointSetGen.h>
 
 #include <cmath>
 #include <chrono>
@@ -23,7 +23,7 @@
 #include <vector>
 #include <limits>  // for std::numeric_limits
 
-using Point = CGAL::Epick_d< CGAL::Dynamic_dimension_tag>::Point_d;
+// using Point = CGAL::Epick_d< CGAL::Dynamic_dimension_tag>::Point_d;
 using Filtration_value = Fake_simplex_tree::Filtration_value;
 using Rips_complex = Gudhi::rips_complex::Rips_complex<Filtration_value>;
 
@@ -31,103 +31,10 @@ using Fake_simplex_tree = Gudhi::Fake_simplex_tree ;
 using Simplex_tree = Gudhi::Simplex_tree<>;
 using Vector_of_points = std::vector<Point>;
 
-void program_options(int argc, char * const argv[]
-                     , std::size_t & number_of_points
-                     , double & begin_thresold
-                     , double & steps
-                     , double & end_thresold
-                     , int 	  & repetetions
-                     , char   & manifold
-                     , int 	  & dimension
-                     , std::string & in_file_name
-                     , std::string & out_file_name
-                     ) {
-  namespace po = boost::program_options;
-
-  po::options_description visible("Allowed options", 100);
-  visible.add_options()
-    ("help,h", "produce help message")
-    ("number,n", po::value<std::size_t>(&number_of_points)->default_value(0),
-       "Number of generated point_vector.")
-    
-    ("begin_thresold,b", po::value<double>(&begin_thresold)->default_value(0),
-       "Initial threshold for rips complex.")
-  	("steps,s", po::value<double>(&steps)->default_value(0.1),
-   		"Steps of the threshold")
-    ("end_thresold,e", po::value<double>(&end_thresold)->default_value(1),
-  		"Final threshold for rips complex.")
-      
-    ("repetetions,r", po::value<int>(&repetetions)->default_value(1),
-    	"Num of repetetions of the experiments.")
-  	("manifold,m", po::value<char>(&manifold)->default_value('s'),
-   		"Steps of the threshold")
-       
-    ("dimensions,D", po::value<int>(&dimension)->default_value(2),
-     "Dimension of the manifold.")
-
-    ("input_file_name,i", po::value<std::string>(&in_file_name),
-     "The input file.")
-    ("out_file_name,o", po::value<std::string>(&out_file_name),
-     "The output file.");
-
-  po::options_description all;
-  all.add(visible);
-
-  po::variables_map vm;
-  po::store(po::command_line_parser(argc, argv).
-            options(all).run(), vm);
-  po::notify(vm);
-
-  if (vm.count("help")) {
-    std::cout << std::endl;
-    std::cout << "Computes rips complexes of different threshold values, from 'begin_thresold' to 'end_thresold', with priodic steps of 'steps' from a n random uniform point_vector on a selected manifold, . \n";
-    std::cout << "Strongly collapses all the rips complexes and output the results in out_file. \n";
-    std::cout << "The experiments are repeted 'repete' num of times for each threshold value. \n";
-    std::cout << "type -m for manifold options, 's' for uni sphere, 'b' for unit ball, 'f' for file. \n";
-    std::cout << "type -i 'filename' for Input file option for exported point sample. \n";
-    std::cout << std::endl << std::endl;
-
-    std::cout << "Usage: " << argv[0] << " [options]" << std::endl << std::endl;
-    std::cout << visible << std::endl;
-    std::abort();
-  }
-}
-
-void generate_points_sphere(Vector_of_points& W, int nbP, int dim, int radius)
-{
-	CGAL::Random_points_on_sphere_d<Point> rp(dim, radius);
-	for (int i = 0; i < nbP; i++)
-		W.push_back(*rp++);
-}
-void generate_points_ball(Vector_of_points& W, int nbP, int dim, int radius)
-{
-	CGAL::Random_points_in_ball_d<Point> rp(dim, radius); 
-	for (int i = 0; i < nbP; i++)
-		W.push_back(*rp++);
-}
-
-void generate_points_cube(Vector_of_points& W, int nbP, int dim)
-{
-	CGAL::Random_points_in_cube_d<Point> rp(dim, 6);
-	for (int i = 0; i < nbP; i++)
-		W.push_back(*rp++);
-}
-
-void add_point_vectors(Vector_of_points& V, Vector_of_points& U, int nbP, int dim) // Adds two point vectors of the same size (nbP), by Modifying the first one, V = V+W.
-{
-	for (int i = 0; i < nbP; i++)
-	{
-		std::vector<double> coords;
-		for(int j =0; j< dim; j++)
-			coords.push_back(V[i][j]+U[i][j]); 
-		V[i] = Point(coords);
-	}
-}
-
 
 int main(int argc, char * const argv[])
 {
-	
+	PointSetGen point_generator;
 	std::string out_file_name = "default";
 	std::string in_file_name = "default";
 	std::size_t number_of_points;
@@ -161,7 +68,7 @@ int main(int argc, char * const argv[])
 	int radius  = 1;
 	long originalNumSimpFrmSmplxtr 	= 0;
 
-	program_options(argc, argv, number_of_points, begin_thresold, steps, end_thresold, repetetions, manifold, dimension, in_file_name, out_file_name);
+	point_generator.program_options(argc, argv, number_of_points, begin_thresold, steps, end_thresold, repetetions, manifold, dimension, in_file_name, out_file_name);
     std::ofstream myfileDetl (out_file_name); 
 
     if(manifold == 'f' || manifold =='F')
@@ -204,11 +111,11 @@ int main(int argc, char * const argv[])
 			point_vector = new Vector_of_points();
 
 			if(manifold == 's' || manifold == 'S'){
-				generate_points_sphere(*point_vector, number_of_points, dimension, radius);
+				point_generator.generate_points_sphere(*point_vector, number_of_points, dimension+1, radius);
 				std::cout << number_of_points << " points successfully chosen randomly from "<< dimension <<"-sphere of radius " << radius << std::endl;
 		    }
 			else if(manifold == 'b' || manifold == 'B'){
-				generate_points_ball(*point_vector, number_of_points, dimension, radius); 
+				point_generator.generate_points_ball(*point_vector, number_of_points, dimension, radius); 
 				std::cout << number_of_points << " points successfully chosen randomly from "<< dimension <<"-ball of radius " << radius << std::endl;
 			
 			}
@@ -240,7 +147,7 @@ int main(int argc, char * const argv[])
 			
 			// std::cout << "Formation of the simplex tree began" << std::endl;
 			// auto simplx_tree_form_began = std::chrono::high_resolution_clock::now();
-			rips_complex_from_points.create_complex(*simplexTreeToCnt,dime);
+			// rips_complex_from_points.create_complex(*simplexTreeToCnt,dime);
 			// auto simplx_tree_form_end = std::chrono::high_resolution_clock::now();
 			// auto simplexTreeFormTime = std::chrono::duration<double, std::milli>(simplx_tree_form_end- simplx_tree_form_began).count();
 			
@@ -283,7 +190,7 @@ int main(int argc, char * const argv[])
 			// auto toplex_count_end = std::chrono::high_resolution_clock::now();
 			// auto toplexCountTime = std::chrono::duration<double, std::milli>(toplex_count_end- toplex_count_began).count();
 
-			originalNumSimpFrmSmplxtr 	+= simplexTreeToCnt->num_simplices();
+			// originalNumSimpFrmSmplxtr 	+= simplexTreeToCnt->num_simplices();
 			colNumSimp					+= ((coll_tree->filtration_simplex_range().size()) - 1);
 
 			delete stree;
